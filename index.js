5439133365d6a7dc5b4262befc0b818c99582122
Marcus Eisele <marcus.eisele@gmail.com>
2016-07-18 14:59:24 +0200

    first working mvp

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var hri = require('human-readable-ids').hri;
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -10,32 +12,18 @@ app.get('/', function(req, res){
 
 var clients = new Map();
 
-io.use(function(socket, next){
-	let username = socket.handshake.query.username;
-	console.log(socket.id);
-	console.log(username);
-	if(!username){
-		console.log("error: no username given")
-		next(new Error('no username given'));
-	}else{
-		console.log("handshake successful")
-		clients.set(socket.id, username);
-		next();
-	}
-})
-
 io.on('connection', function(socket){
-  let username = clients.get(socket.id);
-  console.log('a user connected: '+socket.id+' as '+username);
-  let user = {};
-  user.name = username;
+  
+  console.log('a user connected: '+socket.id+' as '+getName(socket));
+  
   console.log(clients.values());
   let arr = Array.from(clients.values());
 
+  socket.emit('name change', getName(socket));
   io.emit('user update', arr);
   
   socket.on('disconnect', function(){
-    console.log('user disconnected:'+username);
+    console.log('user disconnected:'+getName(socket));
     clients.delete(socket.id);
     let arr = Array.from(clients.values());
     io.emit('user update', arr);
@@ -45,11 +33,30 @@ io.on('connection', function(socket){
   	console.log('message: ' + msg);
   	let out = {};
   	out.msg = msg;
-  	out.username = username;
+  	out.username = getName(socket);
     io.emit('chat message', out);
   });
 
+  socket.on('trigger name change', function(username){
+  	console.log('changing name to: ' + username);
+  	clients.set(socket.id, username);
+    socket.emit('name change', username);
+    let arr = Array.from(clients.values());
+    io.emit('user update', arr);
+
+  });
+
 });
+
+var getName = function(socket){
+	if(clients.has(socket.id)){
+		return clients.get(socket.id);
+	}else{
+		let randomName = hri.random();
+		clients.set(socket.id, randomName);
+		return randomName;
+	}
+};
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
