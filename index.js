2b4f10445846e1e95bc02cc19019670bc3cc6bd8
Marcus Eisele <marcus.eisele@gmail.com>
2016-07-18 13:10:04 +0200

    minimum working version with list of users

--- index.js
+++ index.js
@@ -8,19 +8,45 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + 'public/index.html');
 });
 
+var clients = new Map();
+
+io.use(function(socket, next){
+	let username = socket.handshake.query.username;
+	console.log(socket.id);
+	console.log(username);
+	if(!username){
+		console.log("error: no username given")
+		next(new Error('no username given'));
+	}else{
+		console.log("handshake successful")
+		clients.set(socket.id, username);
+		next();
+	}
+})
+
 io.on('connection', function(socket){
-  console.log('a user connected')
+  let username = clients.get(socket.id);
+  console.log('a user connected: '+socket.id+' as '+username);
+  let user = {};
+  user.name = username;
+  console.log(clients.values());
+  let arr = Array.from(clients.values());
+
+  io.emit('user update', arr);
+  
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log('user disconnected:'+username);
+    clients.delete(socket.id);
+    let arr = Array.from(clients.values());
+    io.emit('user update', arr);
   });
 
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  	console.log('message: ' + msg);
+  	let out = {};
+  	out.msg = msg;
+  	out.username = username;
+    io.emit('chat message', out);
   });
 
 });
